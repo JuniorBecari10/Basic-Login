9c3e8a24eef840341b7320d9bbf34a913db11800
Junior <juniorbecari@gmail.com>
2022-12-14 18:52:27 -0300

    Adding login and logout

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, send_file, request, session, redirect
+from flask_session import Session
+
+registers = []
+program_name = "My Website"
 
 app = Flask(__name__)
 
-register = {}
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 
-program_name = "My Website"
+Session(app)
 
 # <p class="apologize">Account not registered.</p>
 
@@ -18,7 +23,7 @@ def style():
 
 @app.route("/")
 def index():
-  if not session.get("login"):
+  if not session.get("login") or session.get("pass"):
     return redirect("/login")
   
   return render_template("/index.html", title=program_name)
@@ -29,26 +34,50 @@ def login():
     name_req = request.form.get("login")
     pass_req = request.form.get("pass")
     
-    print(name_req, pass_req)
-    
     if is_logged(name_req, pass_req):
       session["login"] = name_req
       session["pass"] = pass_req
       
       return redirect("/")
     
-    return render_template("/login.html", title=program_name + " | Login", apologize="<p class='apologize'>Account not registered.</p>")
+    return render_template("/login.html", title=program_name + " | Login")
   
   return render_template("/login.html", title=program_name + " | Login")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+  if request.method == "POST":
+    name_req = request.form.get("login")
+    pass_req = request.form.get("pass")
+    
+    login(name_req, pass_req)
+
+    session["login"] = name_req
+    session["pass"] = pass_req
+    
+    return redirect("/")
+  
   return render_template("/register.html", title=program_name + " | Register")
+
+@app.route("/logout")
+def logout():
+  session["login"] = None
+  session["pass"] = None
+  
+  return redirect("/login")
 
 # ---
 
 def is_logged(login, passw):
+  global registers
+  
+  for reg in registers:
+    if reg[0] == login and reg[1] == passw:
+      return True
+  
   return False
 
 def login(login, passw):
-  pass
+  global registers
+  
+  registers.append((login, passw))
